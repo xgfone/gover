96a2b7abefbd96e047ea1eeead9a348fb7f340e2
xgfone <xgfone@126.com>
2020-04-26 21:02:05 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-COMMIT=$(git rev-parse HEAD)
-VERSION=$(git describe --tags)
+COMMIT=$(git rev-parse HEAD 2>/dev/null)
+VERSION=$(git describe --tags 2>/dev/null)
 BUILD_DATE=$(date +"%s")
 
-go build -ldflags "-X github.com/xgfone/gover.Commit=$COMMIT -X github.com/xgfone/gover.BuildTime=$BUILD_DATE -X github.com/xgfone/gover.Version=$VERSION"
+go build -ldflags "-w -X github.com/xgfone/gover.Commit=$COMMIT -X github.com/xgfone/gover.BuildTime=$BUILD_DATE -X github.com/xgfone/gover.Version=$VERSION"
