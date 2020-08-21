30dbe861931d86bd70923cfa149f89b978ce791c
xgfone <xgfone@126.com>
2020-08-21 22:33:17 +0800

    update the build script

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
+#!/bin/sh
+
+# Golang Environments
+GO111MODULE=on
+GOSUMDB=sum.golang.google.cn
+GOPROXY=https://goproxy.cn,direct
+
+# Build Version Information
 COMMIT=$(git rev-parse HEAD 2>/dev/null)
 VERSION=$(git describe --tags 2>/dev/null)
 BUILD_DATE=$(date +"%s")
 
-go build -ldflags "-w -X github.com/xgfone/gover.Commit=$COMMIT -X github.com/xgfone/gover.BuildTime=$BUILD_DATE -X github.com/xgfone/gover.Version=$VERSION"
+# Golang Build Flags
+BUILD_FLAGS_DATE="-X github.com/xgfone/gover.BuildTime=$BUILD_DATE"
+BUILD_FLAGS_COMMIT="-X github.com/xgfone/gover.Commit=$COMMIT"
+BUILD_FLAGS_VERSION="-X github.com/xgfone/gover.Version=$VERSION"
+BUILD_FLAGS_X="$BUILD_FLAGS_DATE $BUILD_FLAGS_COMMIT $BUILD_FLAGS_VERSION"
+
+# Build App
+go build -ldflags "-w $BUILD_FLAGS_X"
